08aae6fa9904b3e1bfe4db7ab0e88a53049b70a8
artomily <vararakya@gmail.com>
2025-11-27 14:40:47 +0700

    fetch list of pokemons

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,62 @@
-import { Text, View } from "react-native";
-import { Link } from "expo-router";
+import { Text, View, ScrollView, Image } from "react-native";
+import { useEffect, useState } from "react";
+
+interface Pokemon {
+  name: string;
+  image: string;
+  imageBack: string;
+}
 
 export default function Index() {
+  const [pokemons, setPokemon] = useState<Pokemon[]>([]);
+
+  useEffect(() => {
+    fetchPokemons();
+  }, []);
+
+  async function fetchPokemons() {
+    try {
+      const response = await fetch(
+        "https://pokeapi.co/api/v2/pokemon?limit=20"
+      );
+      const data = await response.json();
+
+      //fetch detailed info for each Pokemon in parallel
+      const detailedPokemons = await Promise.all(
+        data.results.map(async (pokemon: any) => {
+          const res = await fetch(pokemon.url);
+          const details = await res.json();
+          return {
+            name: pokemon.name,
+            image: details.sprites.front_default,
+            imageBack: details.sprites.back_default,
+          };
+        })
+      );
+
+      setPokemon(detailedPokemons);
+    } catch (e) {
+      console.error("Error fetching pokemons:", e);
+    }
+  }
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      <Link href="/login">Go to Login</Link>
-    </View>
+    <ScrollView>
+      {pokemons.map((pokemon) => (
+        <View key={pokemon.name}>
+          <Text>{pokemon.name}</Text>
+          <View style={{ flexDirection : "row" }}>
+            <Image
+              source={{ uri: pokemon.image }}
+              style={{ width: 150, height: 150 }}
+            />
+            <Image
+              source={{ uri: pokemon.imageBack }}
+              style={{ width: 150, height: 150 }}
+            />
+          </View>
+        </View>
+      ))}
+    </ScrollView>
   );
 }
--- app/login.tsx
+++ app/login.tsx
@@ -3,7 +3,6 @@ import { View, Text } from "react-native";
 export default function LoginScreen() {
   return (
     <View>
-      {" "}
       <Text>Login Screen</Text>
     </View>
   );
